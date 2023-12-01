62a01ee6b4db776d1502fca6e654bf577443e326
Sami Khan <sami999khan999@gmail.com>
2023-12-02 00:16:22 +0600

    project sorted

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -3,7 +3,7 @@ const Project = require("../models/projectModel");
 
 // Get all projectsz
 const getAllProjects = async (req, res) => {
-  const projects = await Project.find({});
+  const projects = await Project.find({}).sort({ createdAt: -1 }); // sorted in decending order
 
   res.status(200).json(projects);
 };
